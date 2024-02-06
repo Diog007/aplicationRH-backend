8eff2b9357dc62dd696bfbe6c8d9202585e2eecf
Diog007 <diogo.mz123@gmail.com>
2024-02-06 15:57:06 -0300

    Project Started

---
+++ src/test/java/com/AplicationRH/appRH/AppRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AplicationRH.appRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
