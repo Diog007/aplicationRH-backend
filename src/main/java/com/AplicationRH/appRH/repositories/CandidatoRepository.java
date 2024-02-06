1c3120642ce88994bd91fdbe1a44c0087cd21385
Diog007 <diogo.mz123@gmail.com>
2024-02-06 19:05:24 -0300

    Repositories created

---
+++ src/main/java/com/AplicationRH/appRH/repositories/CandidatoRepository.java
@@ -0,0 +1,8 @@
+package com.AplicationRH.appRH.repositories;
+
+import com.AplicationRH.appRH.models.Candidato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
+
+}
---
+++ src/main/java/com/AplicationRH/appRH/repositories/VagaRepository.java
@@ -0,0 +1,8 @@
+package com.AplicationRH.appRH.repositories;
+
+import com.AplicationRH.appRH.models.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
+
+}
